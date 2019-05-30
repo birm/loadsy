b699f151b0905e75169f93c1553737fb34b15d20
Ryan Birmingham <birm@rbirm.us>
2019-05-30 14:29:46 -0400

    length param

--- loadsy.js
+++ loadsy.js
@@ -1,1 +1,21 @@
-console.log("hi")
+var http = require('http');
+var url = require('url');
+const PORT = 8181
+var srv = http.createServer(function(req, res){
+  var url_parts = url.parse(req.url, true);
+  var query = url_parts.query;
+  res.setHeader('Content-Type', 'application/json');
+  res.write("[")
+  res.write(JSON.stringify(query))
+  let len = query['l'] || query['len'] || 10
+  for (let i=0;i<len-1;i++){
+    res.write(",")
+    res.write(JSON.stringify(query))
+  }
+  res.write("]")
+  res.end()
+});
+
+srv.listen(PORT, function(){
+  console.log("listening on " + PORT)
+})
